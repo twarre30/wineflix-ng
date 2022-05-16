0fff9f133a05d6fc49f900b121db374792de0d26
Tiffany Warren <twarre30@gmail.com>
2022-05-16 14:27:56 -0500

    add logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/logo/logo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.css']
+})
+export class LogoComponent {
+  title = "Wineflix"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
