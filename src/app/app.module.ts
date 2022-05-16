927eaca7fda4017bd6e87fcc9dcb886d6d4589ca
Tiffany Warren <twarre30@gmail.com>
2022-05-16 14:45:17 -0500

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { LogoComponent } from './logo/logo.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    NavigationComponent,
+    UserAvatarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-avatar/user-avatar.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user-avatar',
+  templateUrl: './user-avatar.component.html',
+  styleUrls: ['./user-avatar.component.css']
+})
+export class UserAvatarComponent {
+
+  constructor() { }
+
+
+}
